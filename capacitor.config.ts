263327d3766f02a8b2d94064879744825abd12f1
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2022-10-16 12:21:45 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oni-cards-online',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
