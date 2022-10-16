89614e60cfbf2307addd80daa9735655961e52a4
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2022-10-16 23:32:51 +0200

    Firebase Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard'
+
+const redirectUnauthorizedToLogin = () => 
+  redirectUnauthorizedTo(['/login']);
+
+const redirectLoggedInToHome = () =>
+  redirectLoggedInTo(['/drops']);
 
 const routes: Routes = [
   {
@@ -10,42 +17,50 @@ const routes: Routes = [
   {
     path: 'drops',
     loadChildren: () => import('./pages/drops/drops.module').then( m => m.DropsPageModule),
-    title: "Oni · Drops"
+    title: "Oni · Drops",
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'collection',
     loadChildren: () => import('./pages/collection/collection.module').then( m => m.CollectionPageModule),
-    title: "Oni · Collection"
+    title: "Oni · Collection",
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'social',
     loadChildren: () => import('./pages/social/social.module').then( m => m.SocialPageModule),
-    title: "Oni · Social"
+    title: "Oni · Social",
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'combat',
     loadChildren: () => import('./pages/combat/combat.module').then( m => m.CombatPageModule),
-    title: "Oni · Combat"
+    title: "Oni · Combat",
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'other',
     loadChildren: () => import('./pages/other/other.module').then( m => m.OtherPageModule),
-    title: "Oni · Other"
+    title: "Oni · Other",
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
-    title: "Oni · Login"
+    title: "Oni · Login",
+    ...canActivate(redirectLoggedInToHome)
   },
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
-    title: "Oni · Register"
+    title: "Oni · Register",
+    ...canActivate(redirectLoggedInToHome)
   },
   {
     path: 'forgot-password',
     loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule),
-    title: "Oni · Forgot Password"
+    title: "Oni · Forgot Password",
+    ...canActivate(redirectLoggedInToHome)
   },
 ];
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDsYoZwwnyZJz2zRXwfPC7r265uPiPHr_k",
+    authDomain: "oni-cards-auth.firebaseapp.com",
+    projectId: "oni-cards-auth",
+    storageBucket: "oni-cards-auth.appspot.com",
+    messagingSenderId: "695665333022",
+    appId: "1:695665333022:web:98503dbb808d0224c96f0f",
+    measurementId: "G-0D5F7WXWEC"
+  }
 };
 
 /*
